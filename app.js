ca5a5c88b8260204b1e5106bd753361931d5facd
Rahul Kumar <rahulkumarpahwa@gmail.com>
2023-12-08 15:09:50 +0530

    adding starting files

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const app = express();
+app.engine("ejs", ejsMate); 
+
+
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname ,"/public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get ("/", (req,res)=>{
+res.send("this is root!");
+});
+
+app.get("/homely", (req,res)=>{
+// res.send(" this is the main page");
+res.render("main/homely.ejs");
+});
+
+app.get("/homely/get-involved", (req, res) => {
+  res.send(" this is the get-involved page");
+});
+
+app.get("/homely/about", (req, res) => {
+  res.send(" this is the about page");
+});
+
+app.get("/homely/donate", (req, res) => {
+  res.send(" this is the donate page");
+});
+
+app.listen(8080, ()=>{
+    console.log("server is listening at port 8080");
+});
+
